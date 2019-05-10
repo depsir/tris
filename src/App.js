9eaffc3683e430a687b04475c68958b6adfff675
Francesco Sacchi <depsir@gmail.com>
2019-05-10 17:18:52 +0200

    Toggling moves

--- src/App.js
+++ src/App.js
@@ -3,25 +3,36 @@ import './App.css';
 import Board from "./Board";
 import {toPosition} from "./lib";
 
-function getNextBoard(board, position, move){
+function getNextBoard(board, position, move) {
     const nextBoard = [...board];
     nextBoard[position] = move;
     return nextBoard
 }
 
-function App() {
-    const [board, setBoard] = useState(["","X", "O", "", "", "", "O", "", ""])
+function toggleMove(move) {
+    return move === 'X' ? 'O' : 'X';
+}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Play tris!
-        </p>
-          <Board className={"trisBoard"} data={board} move={(row, column) => setBoard(getNextBoard(board, toPosition(row, column), "X"))}/>
-      </header>
-    </div>
-  );
+function App() {
+    const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+    const [move, setMove] = useState("X");
+
+    const getMove = (row, column) => {
+        setBoard(getNextBoard(board, toPosition(row, column), move));
+        setMove(toggleMove(move))
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Play tris!
+                </p>
+                <Board className={"trisBoard"} data={board}
+                       move={getMove}/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
